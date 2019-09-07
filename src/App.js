6351f324644c0d8b6b19ee8dde842da5b4df2c40
Aksonov Danylo <directxinbox@gmail.com>
2019-09-07 22:13:10 +0300

    added DropDown and json-file, hotel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux'
+import {connect, Provider} from 'react-redux'
+import hotelList from './hotel'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialState = {
+    hotelList: [hotelList],
+    dropDown: true
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'SOME_ACTION':
+            return state;
+        case 'TOGGLE_DROPDOWN':
+            return {...state, dropDown: action.amount};
+        default:
+            return state;
+    }
 }
+
+const store = createStore(reducer, initialState);
+
+const Increment = (amount) => {
+    return {
+        type: 'SOME_ACTION',
+        amount: `${amount}`
+    }
+};
+
+const ToggleDropDown = (amount) =>{
+    return{
+        type: 'TOGGLE_DROPDOWN',
+        amount: amount
+    }
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.showDropDown = this.showDropDown.bind(this);
+    }
+
+    componentDidMount() {
+        store.subscribe(() => {
+            this.forceUpdate()
+        })
+    }
+
+    showDropDown() {
+        store.dispatch(ToggleDropDown(Boolean(this.text.value)))
+    }
+
+    render() {
+        console.log(hotelList);
+        return (
+            <React.Fragment>
+                <input onChange={this.showDropDown} type="text" ref={input => this.text = input}/>
+                <Provider store={store}>
+                    <WrapComponent />
+                </Provider>
+
+            </React.Fragment>
+        )
+    }
+}
+
+
+
+
+
+class DropDown extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        return(
+            <div>{
+
+            }
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) =>{
+    return{
+        dropDown: state.dropDown,
+        hlList: state.hotelList
+    }
+};
+
+const WrapComponent = connect(mapStateToProps)(DropDown);
 
 export default App;
